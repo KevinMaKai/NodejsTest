4ce3b9421acf7336e21109cf4d9db78ecf925f9d
makai5 <makai5@jd.com>
2017-11-21 11:23:34 +0800

    nodejs practice12

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ fs.readFile('input.txt',function(err,data){
 console.log("程序执行结束");
 */
 // 引入 events 模块
-var events = require('events');
+/*var events = require('events');
 // 创建 eventEmitter 对象
 var eventEmitter = new events.EventEmitter();
 // 创建事件处理程序
@@ -31,5 +31,15 @@ eventEmitter.on('data_received',function(){
 
 eventEmitter.emit('connection');
 
-console.log("程序执行完毕。");
+console.log("程序执行完毕。");*/
 
+
+var fs = require('fs');
+fs.readFile('input1.txt',function(err,data){
+	if(err){
+		console.log(err.stack);
+		return;
+	}
+	console.log(data.toString());
+});
+console.log("程序执行完毕。");
